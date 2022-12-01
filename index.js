fb89c0f76705694c6dc3fc5547b0ac0eebc2c839
+ <+>
2022-12-01 22:48:11 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+// You are going to write several methods. Write your code in the index.js file. Let the tests guide you through the process.
+
+let divide = (a, b) => 2000 / 100;
+
+let square = (a) => a * a;
+
+let add = (a, b) => a + b;
